b0c186a9b184a224461d133697c1e02a0e5250b5
Akash-152000 <akash152000akash@gmail.com>
2024-01-30 08:58:28 +0530

    Added utility functions

--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@
 import dotenv from 'dotenv'
 import express from 'express'
 import connectDB from './db/index.js'
+import { app } from './app.js'
 
 dotenv.config({
     path:'./env'
 })
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.port || 8000,()=>{
+        console.log(`Server is running on port ${process.env.port}`)
+    })
+})
+.catch((err)=>{
+    console.log("Mongodb connection failed !!!", err)
+})
 
